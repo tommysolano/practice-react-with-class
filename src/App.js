d3e5592620c0b91eb3880c8ebf89ec572b53a4e0
tommy <tommysolano18@hotmail.com>
2021-10-15 03:30:05 -0500

    entendiendi componentdidmount componentdidupdate y componentwillunmount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { Component } from "react"
 
 class Button extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount")  
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("componentDidUpdate", prevProps, prevState)
+  }
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount", this.props, this.state)
+  }
+
   render(){
     console.log("ejecutando metodo render de Button")
     return(
-      <button>
+      <button onClick={() => this.setState({prop: 1})}>
         Enviar
       </button>
     )
@@ -19,7 +35,9 @@ class App extends Component {
     return (
       <div>
         <p>Hola mundo</p>
-        <Button/>
+        {this.state.valor === 3
+        ? <Button chanchito="feliz"/>
+        : null}
         <button className={this.state.valor} onClick={() => this.setState({ valor: 2})}>
           Enviar en app
         </button> 
